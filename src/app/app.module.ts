22293bbef311f34c73d29495544e6890537c542c
Shaun Wassell <shaun@trove.ai>
2021-08-11 13:10:54 -0400

    Completed template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BackButtonComponent } from './back-button/back-button.component';
+import { SmallXComponent } from './small-x/small-x.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BackButtonComponent,
+    SmallXComponent
   ],
   imports: [
     BrowserModule,
